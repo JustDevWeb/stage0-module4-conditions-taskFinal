c7f05cf38a0f15920530b45f115e4639e95ec07e
JustDevWeb <zm91672@gmail.com>
2023-05-30 13:47:57 +0800

    module4-taskFinal done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        boolean isLeap;
+        if(year > 0) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0 && year % 400 == 0) {
+                    isLeap = true;
+                } else if (year % 100 != 0) {
+                    isLeap = true;
+                } else {
+                    isLeap = false;
+                }
+            } else {
+                isLeap = false;
+            }
+        } else {
+            System.out.println("invalid date");
+            return;
+        }
 
+        switch (month) {
+            case 1,3,5,7,8,12 -> {System.out.println(31);}
+            case 2 -> {
+                if(!isLeap){
+                    System.out.println(28);
+                }else {
+                    System.out.println(29);
+                }
+            }
+            case 4,6,9,11 -> {
+                System.out.println(30);
+            }
+            default ->
+                System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if(divider != 0){
+            int division = dividend / divider;
+            int multiplyResult = division * divider;
+
+            if (multiplyResult == dividend){
+                System.out.println("can be divided completely");
+            }else {
+                System.out.println("cannot be divided completely");
+            }
+        }else {
+            System.out.println("division by zero");
+        }
     }
 }
